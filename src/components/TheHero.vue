24e51572b6e1290084ac2ebfc306aa1519188eac
rashcsve <rashcsve@fel.cvut.cz>
2019-11-26 18:47:23 +0100

    :sparkles: Add hero compote

---
+++ src/components/TheHero.vue
@@ -0,0 +1,101 @@
+<template>
+  <header class="the-hero">
+    <div class="the-hero__blur">
+      <div class="the-hero__blur-background"></div>
+      <div class="the-hero__info">{{ perex }}</div>
+      <!-- Add Button -->
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      perex: "Giraffes are cool!"
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/styles/_variables.scss";
+@import "../assets/styles/_mixins.scss";
+
+.the-hero {
+  text-align: center;
+}
+
+.the-hero__image {
+  background: lighten(black, 40%);
+  width: 100%;
+  background-size: cover;
+  background-position: center;
+}
+
+.the-hero__blur {
+  margin-top: -140px;
+  overflow: hidden;
+  position: relative;
+  padding: 56px 0 64px;
+  background-color: $color-basic;
+
+  @include create-box-shadow-blur();
+
+  @media #{$media-min-tablet} {
+    margin: -15% 2.5% 0 2.5%;
+    height: auto;
+    padding: 32px;
+  }
+}
+
+.the-hero__blur-background {
+  position: absolute;
+  left: -5%;
+  top: -10%;
+  width: 110%;
+  height: 120%;
+  background-image: url("../assets/images/hero.png");
+  background-position: center;
+  background-size: cover;
+  opacity: 0.25;
+  filter: blur(20px);
+
+  &::before {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    content: "";
+  }
+}
+
+.the-hero__info {
+  color: #ffffff;
+  font-size: 24px;
+  line-height: 32px;
+  margin: auto;
+  position: relative;
+  width: 528px;
+  margin-bottom: 24px;
+
+  @media #{$media-min-tablet} {
+    font-size: 16px;
+    line-height: 24px;
+    width: 345px;
+    margin-bottom: 20px;
+  }
+
+  @media screen and (max-width: 600px) {
+    padding: 0;
+    width: auto;
+    line-height: 20px;
+  }
+}
+
+.the-hero__button {
+  position: relative;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="home">
     <the-navigation @show-modal="modalIsOpened" />
-    <div :class="{'home-container':true, 'home-container--modal': showModal}">
-      <img src="../assets/images/hero.png" />
+    <div :class="{'home-container':true, 'home-container__modal': showModal}">
+      <img src="../assets/images/hero.png" alt="Hero Image" class="home-container__image" />
+      <div class="wrapper">
+        <the-hero />
+      </div>
     </div>
   </div>
 </template>
@@ -10,10 +13,11 @@
 <script>
 // @ is an alias to /src
 import TheNavigation from "../components/TheNavigation";
+import TheHero from "../components/TheHero";
 
 export default {
   name: "home",
-  components: { TheNavigation },
+  components: { TheNavigation, TheHero },
   data() {
     return {
       showModal: false
@@ -33,14 +37,37 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss">
+@import "../assets/styles/_variables.scss";
+
 img {
   width: 100%;
 }
 
-.home-container--modal {
-  overflow: hidden;
-  filter: blur(20px);
-  margin-top: 144px;
+.home-container {
+  &__modal {
+    overflow: hidden;
+    filter: blur(20px);
+    margin-top: 144px;
+  }
+
+  &__image {
+    background: lighten(black, 40%);
+    width: 100%;
+    background-size: cover;
+    background-position: center;
+  }
+}
+
+// To Do: compote or style Wrapper
+.wrapper {
+  padding: 0 64px;
+  @media #{$media-max-tablet} {
+    padding: 0 64px;
+  }
+
+  @media #{$media-phone-L} {
+    padding: 0 16px;
+  }
 }
 </style>
