b36c295cf94c06c9e68856e2d15418190980a181
rashcsve <rashcsve@fel.cvut.cz>
2020-01-28 14:00:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 let express = require('express');
 let path = require('path');
+let history = require('connect-history-api-fallback');
 let serveStatic = require('serve-static');
 
 app = express();
-app.use(serveStatic(__dirname + '/dist'));
+
+// Use a fallback for non-root routes (required for Vue router)
+//   NOTE: History fallback must be "used" before the static serving middleware!
+app.use(history({
+    // OPTIONAL: Includes more verbose logging
+    verbose: true
+}))
+
+// Serve static assets from the build files (images, etc)
+app.use(serveStatic(path.join(__dirname, '/dist')));
 
 let port = process.env.PORT || 5000;
 
