8f8ca60c95bacfd84e98e0fec81f8f291d1980f9
Svetlana Rashchupkina <svetlana.rashchupkina@gmail.com>
2020-02-17 16:44:47 +0100

    :bug: Update file name

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import { currency } from '@/services/Currency';
+import { currency } from '@/services/currency';
 
 Vue.config.productionTip = false;
 Vue.filter('currency', currency);
