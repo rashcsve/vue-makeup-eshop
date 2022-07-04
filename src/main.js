b60e500b5da7cc5d1c66d696a08fb2d5443c131b
Svetlana Rashchupkina <svetlana.rashchupkina@gmail.com>
2022-07-04 10:18:27 +0200

    👷  Remove @vue/compat and switch to Vue 3 proper

--- src/main.js
+++ src/main.js
@@ -1,12 +1,7 @@
-import { createApp, configureCompat } from "vue";
+import { createApp } from "vue";
 import App from "./App.vue";
 import { router } from "./router";
 import { store } from "./store";
-
-configureCompat({
-  MODE: 3,
-  ATTR_FALSE_VALUE: false,
-});
 
 const app = createApp(App);
 app.use(store);
